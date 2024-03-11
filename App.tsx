f01d0275de8c8f24a00d40aebf9f6bfd9a00b809
Renan Henrique <renamcomn@Renans-Mac-mini.local>
2024-03-11 17:33:19 -0300

    initial configurations

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, Text } from 'native-base';
+import { THEME } from './src/theme';
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
+import { Splash } from './src/screens/Splash';
 
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <NativeBaseProvider theme={THEME}>
+    <StatusBar 
+      style='light' 
+      backgroundColor='transparent' 
+      translucent
+    />
+    {fontsLoaded ? <Splash/> : <Text>Carregando</Text> }
+  </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Splash.tsx
@@ -0,0 +1,9 @@
+import { Box, Text } from "native-base"
+
+export const Splash = () => {
+    return(
+        <Box flex={1} justifyContent={'center'} alignItems={'center'}>
+            <Text>Splash</Text>
+        </Box>
+    )
+}
